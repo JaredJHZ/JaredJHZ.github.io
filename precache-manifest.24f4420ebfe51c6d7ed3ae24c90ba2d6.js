aa7ad67a08a1cb16e35e77188c14977a981351cc
jaredhz <jaredhz2597@outlook.com>
2020-10-06 14:47:05 -0500

    added

---
+++ precache-manifest.24f4420ebfe51c6d7ed3ae24c90ba2d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9f962a4cd3be0b8906608857363581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "793901de7d27f160b2e9",
+    "url": "/static/css/2.ce4a2144.chunk.css"
+  },
+  {
+    "revision": "816bed7dca39f66eed66",
+    "url": "/static/css/main.005208c4.chunk.css"
+  },
+  {
+    "revision": "793901de7d27f160b2e9",
+    "url": "/static/js/2.2a967bf1.chunk.js"
+  },
+  {
+    "revision": "85f90cf37ae100de945521997b1618a6",
+    "url": "/static/js/2.2a967bf1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "816bed7dca39f66eed66",
+    "url": "/static/js/main.f351ea35.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "6e50041db0b68353fb76b4e9dbd4a9d0",
+    "url": "/static/media/jared.6e50041d.jpg"
+  }
+]);
